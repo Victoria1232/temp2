7a3bad9a480cbe01ccf15ae891a0ae14ad44765c
mfgperez <mfgperez@10.211.55.3>
2023-06-17 15:00:01 -0600

    Created Week6 Lab jsps,servlets

---
+++ src/java/servlets/ShoppingListServlet.java
@@ -0,0 +1,44 @@
+
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author mfgperez
+ */
+public class ShoppingListServlet extends HttpServlet {
+
+   @Override
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { 
+       
+       
+        
+    
+        getServletContext().getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+        
+   
+       
+   }
+   
+    @Override
+   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+   
+ 
+  
+       
+    
+         getServletContext().getRequestDispatcher("/WEB-INF/shoppingList.jsp").forward(request, response);
+         return;
+         
+   
+      
+        
+   }
+}
---
+++ web/WEB-INF/register.jsp
@@ -0,0 +1,24 @@
+<%-- 
+    Document   : register
+    Created on : Jun 17, 2023, 2:45:57 PM
+    Author     : mfgperez
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Register Page</title>
+    </head>
+    <body>
+        <h1>Shopping List</h1>
+        
+        <form method="post" action="ShoppingList">
+            Username: <input type="text" name="username"><br>
+            <input type="submit" value="Register Name">
+            
+        </form>
+        
+    </body>
+</html>
